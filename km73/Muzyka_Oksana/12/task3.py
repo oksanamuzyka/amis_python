a08fb2414b07c9568671ae8132ba672407b6e0b4
Оксана Музика <32312185+oksanamuzyka@users.noreply.github.com>
2017-12-04 18:11:55 +0200

    Delete task3.py

--- km73/Muzyka_Oksana/12/task3.py
+++
@@ -1,8 +0,0 @@
-a = float(input('Enter number: '))
-n = int(input('Enter degree of number: '))
-def power(a, n):
-    if n == 0:
-        return 1
-    else:
-        return a * power(a, n - 1)
-print(power(a, n))
